0e5b54cd0eca1f5c0c4b0db8162a0f989fece63e
Kizito Njoku <nkizito57@gmail.com>
2020-04-02 17:16:21 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -19,7 +19,7 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" onChange={this.handleFirstName} name="message" id="message" />
       </div>
     );
   }
